d01bfbe26f521c31da49be4630e0d032b98cfacb
Tal Liron <tal.liron@gmail.com>
2020-12-10 15:31:26 -0600

    Implement "bytes" primitive type

--- tosca/grammars/tosca_v2_0/bytes.go
+++ tosca/grammars/tosca_v2_0/bytes.go
@@ -1,11 +1,24 @@
 package tosca_v2_0
 
 import (
+	"github.com/tliron/kutil/util"
 	"github.com/tliron/puccini/tosca"
 )
 
+//
+// Bytes
+//
+
 // tosca.Reader signature
 func ReadBytes(context *tosca.Context) tosca.EntityPtr {
-	// TODO
-	return nil
+	var bytes []byte
+
+	if b64 := context.ReadString(); b64 != nil {
+		var err error
+		if bytes, err = util.FromBase64(*b64); err != nil {
+			context.ReportValueMalformed("bytes", "invalid base64")
+		}
+	}
+
+	return bytes
 }
